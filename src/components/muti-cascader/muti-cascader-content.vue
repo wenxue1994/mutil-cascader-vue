7a635e2c805369a6c04b73fddb44bf7e1881d26f
wenxue1994 <61772054+wenxue1994@users.noreply.github.com>
2024-01-08 14:30:04 +0800

    Update muti-cascader-content.vue

--- src/components/muti-cascader/muti-cascader-content.vue
+++ src/components/muti-cascader/muti-cascader-content.vue
@@ -15,8 +15,8 @@
                       :indeterminate='item.little'></Checkbox>
             <span class='check_label' @click='handleChildrenDatas(item,index,item.check)'>{{ item.label }}</span>
           </span>
-          <span class='icon'>
-            <Icon v-show='item.children' type="ios-arrow-forward" />
+          <span class='icon' @click='handleChildrenDatas(item,index,item.check)'>
+            <Icon v-show='item.children&&item.children.length>0' type="ios-arrow-forward" />
           </span>
         </li>
       </ul>
@@ -54,15 +54,17 @@ export default {
       childrenDatas: [],
       selectedDatas: [],
       selectedIndex: -1,
-      childrenCheck: false
+      childrenCheck: false,
     }
   },
   computed: {
+    //是否全选中
     isCheckAll () {
       return this.checkArrayAll(this.entites)
     }
   },
   methods: {
+    //检查里面是否有flase的，如果没有就是全选，如果有就不是全选
     checkArrayAll (datas) {
       for (let i = 0; i < datas.length; i++) {
         if (!datas[i].check) {
@@ -71,30 +73,39 @@ export default {
       }
       return true
     },
+    //检查是否选择横杆
     checkArrayLittle (datas) {
       for (let i = 0; i < datas.length; i++) {
-        if (datas[i].check) {
+        if (datas[i].check || datas[i].little) {
           return true
         }
       }
       return false
     },
+    //全选按钮
     handleCheckAll (checked) {
       this.entites = this.entites.map(item => {
         item.check = checked
         item.little = false
-        if (item.children && item.children.length) {
-          this.childrenCheck = checked
-          item.children.map(_item => {
-            _item.check = checked
-            _item.little = false
-            return _item
-          })
-        }
+        //递归修改
+        this.handleCheckAllChildren(item,checked)
         return item
       })
+      //需要更新父组件
       this.updateToParent()
     },
+    //递归修改
+    handleCheckAllChildren(parentItem,checked){
+      if (parentItem.children && parentItem.children.length) {
+        parentItem.children = parentItem.children.map(childItem => {
+          childItem.check = checked;
+          childItem.little = false;
+          this.handleCheckAllChildren(childItem, checked);
+          return childItem;
+        });
+      }
+    },
+    //点击展示右边
     handleChildrenDatas (item, index, checked) {
       this.selectedIndex = index
       if (item.children && item.children.length) {
@@ -102,22 +113,41 @@ export default {
         this.childrenTitle = item.label
       }
     },
+    //选中那个数据，现在会递归到最底部，而且会把横杆改为false
     handleSelect (checked, item, index) {
+      //如果是选中变成未选中，就要把横线改为false
       if (checked)item.little = false
+      //赋值当前选中的selectedIndex
       this.selectedIndex = index
-      if (item.children && item.children.length) {
-        item.children = item.children.map(_item => {
-          _item.check = checked
-          return _item
-        })
-        this.childrenDatas = item.children
-        this.childrenTitle = item.label
-      }
+      if (checked.children && checked.children.length) {
+        // 设置后续展示需要用到的值
+          this.childrenDatas = parentItem.children;
+          this.childrenTitle = parentItem.label;
+      }      
+
+      // 递归处理子节点,2024/1/8
+      this.handleChildren(item, checked);    
       this.updateToParent()
     },
+    handleChildren(parentItem, checked) {
+      if (parentItem.children && parentItem.children.length) {
+        parentItem.children = parentItem.children.map(childItem => {
+          childItem.check = checked;
+          childItem.little = false;
+          this.handleChildren(childItem, checked);
+          return childItem;
+        });
+      }
+  },    
+    //更新这个
     childUpdate (childDatas) {
+      
+      //把它更新到当前子
       this.entites[this.selectedIndex].children = Object.assign([], childDatas)
+      //判断是否全选
       this.entites[this.selectedIndex].check = this.checkArrayAll(childDatas)
+
+      //判断是否选择横杆
       if (this.entites[this.selectedIndex].check) {
         this.entites[this.selectedIndex].little = false
       } else {
@@ -125,6 +155,7 @@ export default {
       }
       this.updateToParent()
     },
+    //传给父组件，一级一级传，直到顶部
     updateToParent () {
       this.$emit('update', this.entites)
     }
@@ -177,6 +208,7 @@ li {
           .check_label {
             display: inblock;
             width: 80;
+            cursor: pointer;
           }
         }
         // .icon {
